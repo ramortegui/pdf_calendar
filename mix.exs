492aabc3f13fc4b39932b219c2798c35ce7b3dcd
Ruben Amortegui <ruben.amortegui@gmail.com>
2019-01-05 19:24:38 -0700

    Update pcal dependency.

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule PdfCalendar.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:pcal, git: "https://github.com/ramortegui/pcal.git", tag: "v0.1.1"}
+      {:pcal, git: "https://github.com/ramortegui/pcal.git", tag: "v0.1.2"}
     ]
   end
 end
